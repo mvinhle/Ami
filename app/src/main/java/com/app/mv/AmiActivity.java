78ee3c36ed7e5aa22c6f0911a658c8dfff43d9b1
Lê Minh Vinh <mvinhle@outlook.com>
2018-07-26 21:55:07 +0700

    file chat của ami

--- app/src/main/java/com/app/mv/AmiActivity.java
+++ app/src/main/java/com/app/mv/AmiActivity.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 
 public class AmiActivity extends AppCompatActivity {
 
-    int trust = 0;
-    int index_clothes  = 2;
-    int chat = 0;
-    String sYou = "ông", sI = "tui", sNI = "Ami";
+    int trust    = 0;
+    int clothes  = 3;
+    int chat     = 1;
+    String sYou = "anh", sI = "em", sNI = "Ami", sNYou = "Vinh";
 
     HelpData helpData;
 
@@ -25,14 +25,14 @@ public class AmiActivity extends AppCompatActivity {
     int bodyChange,  hairchange,  eyeChange,  eyebrowChange,   clothesChange,  glassChange,  featureChange,   mouthChange;
 
     final int BODY_DEFAULT    = 1; // body.length;		// HAIR DEFAULT = BODY DEFAULT = 1
-    int CLOTHES_DEFAULT = index_clothes; 		// [0, <= 63] = 64?
+    final int CLOTHES_DEFAULT = 64;              		// [0, 63]
     final int EYE_TINY        = 2;
-    final int EYE_BIG         = EYE_TINY + 4; 		// [indexTinyDefault,5] = 6 (4 is length of eyeBig)
-    final int EYE_FUN         = EYE_BIG + 3; 		// [indexTinyBig, 9] = 10
+    final int EYE_BIG         = EYE_TINY + 4; 	    	// [indexTinyDefault,5] = 6 (4 is number of eyeBig)
+    final int EYE_FUN         = EYE_BIG + 3; 	    	// [indexTinyBig, 9] = 10
     final int EYEBROW_DEFAULT = 1;
     final int EYEBROW_ANGRY   = EYEBROW_DEFAULT + 1; 	// [EYEBORW_DEFAULT, 1] = 2
     final int EYEBROW_SAD     = EYEBROW_ANGRY + 1;  	// [EYEBROW_ANGRY, 2] = 3
-    final int GLASS_DEFAULT   = 4; // glass.length; 	    	// [0, 3] = 4
+    final int GLASS_DEFAULT   = 4; // glass.length; 	// [0, 3] = 4
     final int FEATURE_FUN     = 9;
     final int FEATURE_ANGRY   = FEATURE_FUN + 9; 	    // [FEATURE_FUN, 17] = 18
     final int FEATURE_SAD     = FEATURE_ANGRY + 2; 	    // [FEATURE_FUN, 19] = 20
@@ -41,9 +41,10 @@ public class AmiActivity extends AppCompatActivity {
     final int MOUTH_ANGRY     = MOUTH_FUN + 6; 		    // [MOUTH_FUN, 44] = 45
     final int MOUTH_SAD       = MOUTH_ANGRY + 6; 	    // [MOUTH_ANGRY, 50] = 51
     final int MOUTH_SUDDENT   = MOUTH_SAD + 6; 		    // [MOUTH_SAD, 56] = 57
-    final int TEST_WIN        = 100;
-    final int TEST_LOST       = -20;
-    final int CHAT            = 2;
+    final int TEST_WIN        = 100;  //x10 up 1 chat
+    final int TEST_LOST       = -25;  //x40 down 1 chat
+    final int CHAT_ABOUT      = 10;   //chat random [chat, chat + 10) // about 10 chat
+    final int CHAT_ADD        = 10;   //x100 up 1 chat
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -118,6 +119,7 @@ public class AmiActivity extends AppCompatActivity {
                 imageMouth.setImageResource(mouthChange);
                 imageFeature.setImageResource(featureChange);
                 imageEyebrow.setImageResource(eyebrowDeafault);
+                Log.d("MVLog", "AmiActivity <- setImageAmi ami cảm thấy vui (fun)");
                 break;
             case "Angry":
 //                String eyebrow[]    = getResources().getStringArray(R.array.eyebrow);
@@ -130,6 +132,7 @@ public class AmiActivity extends AppCompatActivity {
                 imageMouth.setImageResource(mouthChange);
                 imageEyebrow.setImageResource(eyebrowChange);
                 imageEye.setImageResource(eyeDefault);
+                Log.d("MVLog", "AmiActivity <- setImageAmi ami cảm thấy giận (ang)");
                 break;
             case "Sad":
 //                String eyebrow[]    = getResources().getStringArray(R.array.eyebrow);
@@ -142,12 +145,14 @@ public class AmiActivity extends AppCompatActivity {
                 imageEyebrow.setImageResource(eyebrowChange);
                 imageFeature.setImageResource(featureChange);
                 imageEye.setImageResource(eyeDefault);
+                Log.d("MVLog", "AmiActivity <- setImageAmi ami cảm thấy buồn (sad)");
                 break;
             case "Suddent":
 //                String mouth[]      = getResources().getStringArray(R.array.mouth);
                 mouthChange = idImageFromName(mouth[helpData.randomRange(MOUTH_SAD, MOUTH_SUDDENT)]);
                 imageMouth.setImageResource(mouthChange);
                 imageEye.setImageResource(eyeDefault);
+                Log.d("MVLog", "AmiActivity <- setImageAmi ami cảm thấy bất ngờ (sud)");
                 break;
             case "Default":
                 imageBody.setImageResource(bodyDefault);
@@ -158,6 +163,7 @@ public class AmiActivity extends AppCompatActivity {
                 imageGlass.setImageResource(glassDefault);
                 imageFeature.setImageResource(featureDefault);
                 imageMouth.setImageResource(mouthDefault);
+                Log.d("MVLog", "AmiActivity <- setImageAmi ami cảm thấy bình thường (def)");
                 break;
             default:
                 Log.d("MVLog", "AmiActivity <- setImageAmi không nhận được trạng thái");
@@ -176,7 +182,8 @@ public class AmiActivity extends AppCompatActivity {
         if(text.contains("[ang]")){setImageAmi("Angry"); text = helpData.removeSubString(text, "[ang]");}
         if(text.contains("[sad]")){setImageAmi("Sad"); text = helpData.removeSubString(text, "[sad]");}
         if(text.contains("[sud]")){setImageAmi("Suddent"); text = helpData.removeSubString(text, "[sud]");}
-        text = text.replaceAll("[you]", sYou).replaceAll("[i]", sI).replaceAll("[ni]", sNI);
+        if(text.contains("[def]")){setImageAmi("Default"); text = helpData.removeSubString(text, "[def]");}
+        text = text.replaceAll("[you]",sYou).replaceAll("[snyou]", sNYou).replaceAll("[i]", sI).replaceAll("[ni]", sNI);
         textAmiChat.setText(text);
     }
 }
